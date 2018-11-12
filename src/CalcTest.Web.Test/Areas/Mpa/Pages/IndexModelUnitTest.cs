0f5ea4ae9088c67d0a81c6d2e7031f16b4a60673
Roberto Carlos <roberto.dsilva@fgv.br>
2018-11-12 13:47:33 -0200

    Adicionando testes unitários da site de integração

---
+++ src/CalcTest.Web.Test/Areas/Mpa/Pages/IndexModelUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using CalcTest.Web.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcTest.Web.Areas.Mpa.Pages
+{
+    [TestClass]
+    public class IndexModelUnitTest
+    {
+        [TestMethod]
+        public async Task Quando_Executado_Retorna_Calculo_Juros_Composto()
+        {
+            //Arrange
+            var srv = new CalculadoraHttpServiceFake();
+            var pageModel = new IndexModel(srv);
+            //Código da competição
+            pageModel.Calculo = new ViewModels.CalculoViewModel();
+            pageModel.Calculo.ValorInicial = 100;
+            pageModel.Calculo.Meses = 5;
+            //Act
+            await pageModel.OnPostAsync();
+            //Assert
+            Assert.AreEqual(pageModel.Calculo.Resultado, "105,10");
+        }
+
+    }
+}
---
+++ src/CalcTest.Web.Test/Services/CalculadoraHttpServiceFake.cs
@@ -0,0 +1,19 @@
+﻿using CalcTest.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcTest.Web.Services
+{
+    public class CalculadoraHttpServiceFake : ICalculadoraHttpService
+    {
+
+        public async Task<string> CalcularJurosAsync(double valorInicial, int meses)
+        {
+            var calculo = new Calculo(valorInicial, meses);
+            calculo.Calcula(0.01);
+            return await Task.FromResult(calculo.ToString());
+        }
+    }
+}
