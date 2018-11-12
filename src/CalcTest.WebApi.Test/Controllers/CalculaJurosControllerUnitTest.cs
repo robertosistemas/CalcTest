8d27da07cc369edd1bebebbff6fbb9f9d0720c46
Roberto Carlos da Silva <robertosistemas@hotmail.com>
2018-11-12 01:15:04 -0200

    Adicionando o projeto de teste da web api

---
+++ src/CalcTest.WebApi.Test/Controllers/CalculaJurosControllerUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using CalcTest.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcTest.WebApi.Controllers
+{
+
+    [TestClass]
+    public class CalculaJurosControllerUnitTest : BaseUnitTest
+    {
+
+        [TestMethod]
+        public void Get_Quando_Executado_Retorna_Calculo_Juros_Composto()
+        {
+            //Arrange
+            var calculadoraFinanceiraService = serviceProvider.GetService<ICalculadoraFinanceiraService>();
+            var calculaJurosController = new CalculaJurosController(calculadoraFinanceiraService);
+            //Act
+            var resultado = calculaJurosController.Get(100, 5).Value;
+            //Assert
+            Assert.AreEqual(resultado, "105,10");
+        }
+
+    }
+}
---
+++ src/CalcTest.WebApi.Test/Controllers/ShowMeTheCodeControllerUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcTest.WebApi.Controllers
+{
+    [TestClass]
+    public class ShowMeTheCodeControllerUnitTest : BaseUnitTest
+    {
+
+        [TestMethod]
+        public void Get_Quando_Executado_Retorna_Url_Fonte_Github()
+        {
+            //Arrange
+            var calculaJurosController = new ShowMeTheCodeController();
+            //Act
+            var resultado = calculaJurosController.Get().Value;
+            //Assert
+            Assert.AreEqual(resultado, "https://github.com/robertosistemas/RepositorioTeste01");
+        }
+
+    }
+
+}
