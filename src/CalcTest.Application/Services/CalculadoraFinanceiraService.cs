11c3feba8a613453c5944209225aea4cc9dc81ae
Roberto Carlos da Silva <robertosistemas@gmail.com>
2018-11-12 00:57:29 -0200

    Adicionando camada de regras de negócio da aplicação

---
+++ src/CalcTest.Application/Services/CalculadoraFinanceiraService.cs
@@ -0,0 +1,25 @@
+﻿using CalcTest.Core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcTest.Application.Services
+{
+    public class CalculadoraFinanceiraService : ICalculadoraFinanceiraService
+    {
+
+        private readonly ICalculadoraService _calculadoraService;
+
+        public CalculadoraFinanceiraService(ICalculadoraService calculadoraService)
+        {
+            _calculadoraService = calculadoraService;
+        }
+
+        public CalculoOutputDto CalculaJurosComposto(CalculoInputDto calculoInputDto)
+        {
+            var calculo = new Calculo(calculoInputDto.ValorInicial, calculoInputDto.Meses);
+            var resultado = _calculadoraService.CalculaJurosComposto(calculo);
+            return new CalculoOutputDto(resultado.Resultado);
+        }
+    }
+}
---
+++ src/CalcTest.Application/Services/CalculoInputDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcTest.Application.Services
+{
+    public class CalculoInputDto
+    {
+        public double ValorInicial { get; set; }
+        public int Meses { get; set; }
+
+        public CalculoInputDto(double valorInicial, int meses)
+        {
+            ValorInicial = valorInicial;
+            Meses = meses;
+        }
+    }
+}
