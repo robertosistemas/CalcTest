cdb61d90a6a3c16202f94de37b81b98152ad075f
Roberto Carlos da Silva <robertosistemas@gmail.com>
2018-11-12 00:39:03 -0200

    Adicionado a camada de domínio da aplicação

---
+++ src/CalcTest.Core/Domains/CalculadoraService.cs
@@ -0,0 +1,23 @@
+﻿
+namespace CalcTest.Core.Domains
+{
+    public class CalculadoraService : ICalculadoraService
+    {
+
+        private readonly double _juros;
+
+        public CalculadoraService()
+        {
+            _juros = 0.01;
+        }
+
+        public Calculo CalculaJurosComposto(Calculo calculo)
+        {
+
+            calculo.Calcula(_juros);
+
+            return calculo;
+        }
+
+    }
+}
